6b4c12a0b8053d4150b612fbafc29faf1f6f44ec
siebe-geyskens <siebe-geyskens@hotmail.com>
2024-09-15 21:02:09 +0200

    add post details view

--- src/App.js
+++ src/App.js
@@ -1,37 +1,91 @@
 import { useEffect, useState } from "react";
 
 function App() {
-  const [posts, setPosts] = useState([]); // Clean state with all posts for filtering 
+  const [posts, setPosts] = useState([]); // Clean state with all posts for filtering
   const [filteredPosts, setFilteredPosts] = useState([]); // Filtered posts to render
-  const [searchValue, setSearchValue] = useState(''); // Controlled input element
+  const [searchValue, setSearchValue] = useState(""); // Controlled input element
+  const [selectedPost, setSelectedPost] = useState(null);
 
   useEffect(() => {
     fetchPosts();
-  }, [])
+  }, []);
 
   const handleInputChange = (e) => {
     const searchTerm = e.target.value;
     setSearchValue(searchTerm);
-    const filtered = posts.filter(post => post.title.toLowerCase().includes(searchTerm.toLowerCase()));
+    const filtered = posts.filter((post) =>
+      post.title.toLowerCase().includes(searchTerm.toLowerCase())
+    );
     setFilteredPosts(filtered);
-  }
-  
+  };
+
+  const handleOpenPost = async (post) => {
+    try{
+      const response = await fetch(
+      `https://jsonplaceholder.typicode.com/posts/${post.id}/comments`
+      );
+      const data = await response.json();
+      setSelectedPost({ ...post, comments: data });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleClosePost = () => {
+    setSelectedPost(null);
+  };
+
   const fetchPosts = async () => {
     try {
-      const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/posts"
+      );
       const data = await response.json();
       setPosts(data);
       setFilteredPosts(data);
     } catch (error) {
       console.error(error);
     }
-  }
+  };
 
-  return (
+  const buttonStyle = { cursor: "pointer" };
+
+  return !selectedPost ? (
     <div>
-      <input type="text" value={searchValue} onChange={handleInputChange} placeholder="search post titles" />
-      <ul> 
-        {filteredPosts.map(post => <li key={post.id}><p>{post.title}</p></li>)}
+      <input
+        type="text"
+        value={searchValue}
+        onChange={handleInputChange}
+        placeholder="search post titles"
+      />
+      <ul>
+        {filteredPosts.map((post) => (
+          <li key={post.id}>
+            <span style={{ marginRight: 4 }}>{post.title}</span>
+            <button
+              style={buttonStyle}
+              onClick={() => {
+                handleOpenPost(post);
+              }}
+            >
+              View
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  ) : (
+    <div>
+      <button style={buttonStyle} onClick={handleClosePost}>
+        Close
+      </button>
+      <h1>{selectedPost.title}</h1>
+      <p>{selectedPost.body}</p>
+      <h2>Comments</h2>
+      <ul>
+        {selectedPost.comments.map((comment) => (
+          <li key={comment.id}>{comment.body}</li>
+        ))}
       </ul>
     </div>
   );
